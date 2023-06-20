b7e2fcb4cf664571f8a1f0535340cbb507a35988
seif telmini <133585531+tonnysei@users.noreply.github.com>
2023-06-20 18:25:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,39 +11,19 @@ function playGame(playerMove) {
 
   const computerMove= pickComputerMove();
 
-  if (playerMove === 'scissors') {
-    if (computerMove === 'rock') {
-      result = 'You lose'
-    } else if (computerMove === 'paper') {
-      result = 'You win'
-    } else if (computerMove === 'scissors') {
-      result = 'Tie'
-    }
-
-  } else if (playerMove === 'paper') {
-    if (computerMove === 'rock') {
-      result = 'You win'
-    } else if (computerMove === 'paper') {
-      result = 'Tie'
-    } else if (computerMove === 'scissors') {
-      result = 'You lose'
-    }
-
-  } else if (playerMove === 'rock') {
-    if (computerMove === 'rock') {
-      result = 'Tie'
-    } else if (computerMove === 'paper') {
-      result = 'You lose'
-    } else if (computerMove === 'scissors') {
-      result = 'You win'
-    }
+  if (playerMove === computerMove) {
+    result = 'Tie';
+  } else if ((playerMove === 'rock' && computerMove === 'scissors') || (playerMove === 'paper' && computerMove === 'rock') || (playerMove === 'scissors' && computerMove === 'paper')) {
+    result = 'You win';
+  } else {
+    result = 'You lose';
   }
 
   if (result === 'You win') {
     score.wins += 1;
   } else if (result === 'You lose') {
     score.losses += 1;
-  } else if (result === 'Tie') {
+  } else {
     score.ties += 1;
   }
 
